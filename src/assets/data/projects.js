06cc5ab9a0d2521d12cd4e4799de89327493bbe2
temanchib <mencoba.keberungtungan@gmail.com>
2022-06-15 20:49:55 +0700

    data

--- src/assets/data/projects.js
+++ src/assets/data/projects.js
@@ -10,43 +10,37 @@ const projects = [
   {
     id: uuidv4(),
     name: 'Sunday Service',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image1,
   },
   {
     id: uuidv4(),
     name: 'Community Cell',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image2,
   },
   {
     id: uuidv4(),
     name: 'Discipleship PMK',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image3,
   },
   {
     id: uuidv4(),
     name: 'Bimbingan Pranikah',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image4,
   },
   {
     id: uuidv4(),
     name: 'Sepakat Berdoa',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image5,
   },
   {
     id: uuidv4(),
     name: 'Komunitas Opa & Oma',
-    desc:
-      ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     img: image6,
   },
 ];
