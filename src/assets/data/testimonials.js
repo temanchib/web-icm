b235a8d4f7f0a1b35398715148aae3e997c30539
temanchib <mencoba.keberungtungan@gmail.com>
2022-06-15 20:54:54 +0700

    data

--- src/assets/data/testimonials.js
+++ src/assets/data/testimonials.js
@@ -4,28 +4,28 @@ const testimonials = [
     name: 'Komsel Keluarga',
     title: 'name',
     org: 'phone number',
-    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.',
   },
   {
     id: 2,
     name: 'Komsel Youth',
     title: 'name',
     org: 'phone number',
-    desc: ' It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.',
   },
   {
     id: 3,
     name: 'Komsel Opa / Oma',
     title: 'name',
     org: 'phone number',
-    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.',
   },
   {
     id: 4,
     name: 'Komsel Kega',
     title: 'name',
     org: 'phone number',
-    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+    desc: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop.',
   },
 ];
 export default testimonials;
